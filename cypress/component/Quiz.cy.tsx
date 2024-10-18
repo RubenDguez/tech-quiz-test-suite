871e719b4e715997e355b37d5f7e4eb5d9396fb1
Ruben Dominguez <argenis.dominguez@hotmail.com>
2024-10-18 12:42:55 -0400

    hotfix: add missing tests in component

--- cypress/component/Quiz.cy.tsx
+++ cypress/component/Quiz.cy.tsx
@@ -5,11 +5,13 @@ describe('Quiz component', () => {
     cy.mount(<Quiz />)
   })
   it('renders', () => {
-    
+    cy.get('.btn').should('exist');
+    cy.get('.btn').should('be.visible');
   })
   describe('Start Quiz button', () => {
     it('should render the start quiz button if quiz not started', () => {
       cy.get('.btn').should('exist');
+      cy.get('.btn').should('be.visible');
     })
     it('should have "Start Quiz" as label text', () => {
       cy.get('.btn').should('have.text', "Start Quiz")
